7296222651410877a7c0062fd2f28ea8c70afc6c
Adnane <adnanitoben@gmail.com>
2023-08-30 12:32:39 +0100

    First commit: Layouts and Pages

---
+++ src/pages/Mentors.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from "../components/Layout"
+import Header from '../components/Header'
+   
+function Mentors() {
+  
+    return (
+        <Layout>
+            <Header/>
+            <div className="container">
+                <h2 className="text-center mt-5 mb-3">Mentors </h2>
+            </div>
+        </Layout>
+    );
+}
+   
+export default Mentors;
